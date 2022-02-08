b3ebd1278fa155b1c22116e00d135688a909ac4d
jkevorkian <jkevorkiandiaz@frba.utn.edu.ar>
2022-02-07 21:21:08 -0300

    agrego intento de validacion en el login, sin demasiado exito. Hay que arreglarlo

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
     },
   });
 
+
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,39 @@
 import React from "react";
-import { TextField } from "@mui/material";
-import { Button } from "@mui/material";
+import {TextField} from "@mui/material";
+import {Button} from "@mui/material";
 
 class Login extends React.Component {
-  render() {
-    return (
-      <div>
-        <form>
-          <TextField
-            id="outlined-basic"
-            label="Usuario"
-            variant="outlined"
-            color="primary"
-            required="true"
-          />
-          <br />
-          <br />
-          <TextField
-            id="outlined-basic"
-            label="Contraseña"
-            variant="outlined"
-            color="primary"
-            required="true"
-          />
-          <br />
-          <br />
-          <br />
-          <Button variant="outlined" color="secondary">
-            ( ˘⌣˘)♡(˘⌣˘ )
-          </Button>
-        </form>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <form>
+                    <TextField
+                        id="outlined-basic"
+                        label="Usuario"
+                        variant="outlined"
+                        color="primary"
+                        required="true"
+                    />
+                    <br/>
+                    <br/>
+                    <TextField
+                        id="outlined-basic"
+                        label="Contraseña"
+                        variant="outlined"
+                        color="primary"
+                        required="true"
+                    />
+                    <br/>
+                    <br/>
+                    <br/>
+                    <Button variant="outlined" color="secondary"
+                            onClick={document.querySelector(".usr").value === "sofi" && document.querySelector(".pass").value === "kaede" ? alert("entraste") : alert("no entraste")}>
+                        ( ˘⌣˘)♡(˘⌣˘ )
+                    </Button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Login;
